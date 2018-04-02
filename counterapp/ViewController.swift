69c82323e89d0d1159e42fce6f48330a17af0b88
Vladimir Saldivar <vsaldivarm@gmail.com>
2018-04-02 17:00:24 -0500

    Initial Commit

---
+++ counterapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  counterapp
+//
+//  Created by Vladimir Saldivar on 4/2/18.
+//  Copyright © 2018 spartdark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
